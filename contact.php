aceae29a7f1cec5fe396e995d66617546d5b010a
juanlove <juan.a.urena@gmail.com>
2020-06-11 16:59:23 -0700

    first commit

---
+++ contact.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ *  Contact Form
+ */
+
+
+
+// hello@juanlove.design or juanlovedesign@gmail.com
+$sendTo = 'hello@juanlove.design;'
+
+
+
+
+
+
+// message that will be displayed when everything is OK
+$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
+
+// If something goes wrong, we will display this message
+$errorMessage = 'There was an error while submitting the form. Please try again later';
+
+
+/*
+ *  LET'S DO THE SENDING
+ */
+
+
+// error_reporting( E_ALL & ~E_NOTICE ); // - Debugging on
+error_reporting(0); // - Debugging off
+
+try {
+
+	if ( count( $_POST ) == 0 && ! isset($_POST['username'])&& ! isset($_POST['email'])&& ! isset($_POST['message'])) {
+		throw new \Exception( 'Form is empty' );
+	}
+
+	// an email address that will be in the From field of the email.
+	$from = $_POST['email'] ;
+	// The message send in email
+	$message = $_POST['message'] ;
+	// subject of the email
+    $subject = $_POST['name'];
+
+
+
+	$headers = 'From: ' . $from . "\r\n" .
+	           'Reply-To: ' . $sendTo . "\r\n" .
+	           'X-Mailer: PHP/' . phpversion();
+
+
+
+	// Send email
+	mail( $sendTo, $subject, $message, $headers );
+
+	$responseArray = array('type' => 'success', 'message' => $okMessage);
+} catch ( \Exception $e ) {
+	 $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+}
+
+
+// if requested by AJAX request return JSON response
+if ( ! empty( $_SERVER['HTTP_X_REQUESTED_WITH'] ) && strtolower( $_SERVER['HTTP_X_REQUESTED_WITH'] ) == 'xmlhttprequest' ) {
+	$encoded = json_encode( array( 'status' => true, 'message' => $okMessage ) );
+
+	header( 'Content-Type: application/json' );
+
+	echo $encoded;
+} // else just display the message
+else {
+	$encoded = json_encode( array( 'status' => false, 'message' => $errorMessage ) );
+
+	header( 'Content-Type: application/json' );
+
+	echo $encoded;
+}
